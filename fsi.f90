bd97ff4283e8a1c0176b1958fdf6b10710266885
me <f.nikolakopoulos@gmail.com>
2023-02-18 14:27:50 +0200

    inflow function

---
+++ fsi.f90
@@ -0,0 +1,38 @@
+program main
+FUNCTION Youngs( Model, n, x ) RESULT( s )
+  USE Types
+  TYPE(Model_t) :: Model
+  INTEGER :: n
+  REAL(KIND=dp) :: x,s,s1,s2,s3,xx,yy
+  
+  xx = Model % Nodes % x(n)
+  yy = Model % Nodes % y(n)
+  
+  s =  1.0d0 / SQRT( (xx-11.0)**2 + (yy-4.9)**2 )
+  
+END FUNCTION Youngs
+
+
+FUNCTION inflow( Model, n, x ) RESULT( vin )
+  USE Types
+  TYPE(Model_t) :: Model
+  INTEGER :: n
+  REAL(KIND=dp) :: yy,x,vin,v0,vt
+  
+  xx = Model % Nodes % x(n)
+  yy = Model % Nodes % y(n)
+  
+  v0 = (-(yy**2)+10*yy)/10
+  
+  ! IF(x < 8.0) THEN
+  !   vt = x/8.0
+  ! ELSE
+  !   vt = 1.0
+  ! END IF
+  
+  !  vin = v0*vt
+
+  velocity=sqrt(x)*0.0001*(1 - yy)*yy
+  
+END FUNCTION InFlow
+end program main
